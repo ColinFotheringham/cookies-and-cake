bbb9afcab443534a3c65c6d173415dae5cbdbcef
ColinFotheringham <colinfthrnghm@gmail.com>
2021-11-09 17:32:35 -0500

    added pg dependency

--- src/index.js
+++ src/index.js
@@ -4,6 +4,30 @@ import './index.css';
 import App from './App';
 
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM item');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
