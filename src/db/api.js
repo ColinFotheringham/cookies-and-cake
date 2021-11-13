176722d3f36e70f74a28c7c2871164e4a9b53111
ColinFotheringham <colinfthrnghm@gmail.com>
2021-11-13 16:39:27 -0500

    update

--- src/db/api.js
+++ src/db/api.js
@@ -1,13 +1,12 @@
 const client = require('../db/connection.js');
 
-var items = [];
+
  client.connect();
 
  client.query ('SELECT * FROM item;', async (err, res) => {
    if (err) throw err;
    for (let row of res.rows) {
-    // console.log(JSON.stringify(row));
-       items = ['JSON.stringify(row)'];
+      console.log(JSON.stringify(row));
    }   client.end();
  });
 
