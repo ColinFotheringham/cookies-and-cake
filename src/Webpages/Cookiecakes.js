e1e4ffb2be59d6da315f03c28f44cbade695ed5a
ColinFotheringham <colinfthrnghm@gmail.com>
2021-11-13 16:41:26 -0500

    pushed change

--- src/Webpages/Cookiecakes.js
+++ src/Webpages/Cookiecakes.js
@@ -1,22 +1,5 @@
 // import Nav from './MainNav/MainNav';
 import React from 'react';
-const client = require('../db/connection.js');
-
-var items = [];
-
-client.connect();
-
-client.query ('SELECT * FROM item;', async (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-   console.log(JSON.stringify(row));
-    items = [JSON.stringify(row)];
-
-  }
-  client.end();
-});
-
-
 
 function Cookiecakes() {
   return (
