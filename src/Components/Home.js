47e36179c250e364894def0c00275d0610020dba
brysonprince <59940182+brysonprince@users.noreply.github.com>
2021-12-14 10:35:49 -0700

    Added Carousel and info snippet on home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import Carousel from './mainCarousel'
 
 export const Home = () => {
 
     return (
         <div>  
-        
         <img src="Menu-Pictures/cookiesandc.jpg" className="logo mx-auto d-block banner" alt="The logo of cookies and cake" fluid />
+        <Carousel></Carousel>
+        <h3 id='home-paragraph'>Welcome to Cookies and Cake! I am located in Raymond, AB and would love to make your next custom cake or treat. Don't hesitate to contact me with any questions.</h3>
         </div>
     )
 }
