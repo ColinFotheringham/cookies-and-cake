82df8b06f021abba9f78597b2e3615778ee5d928
colinfthrnghm@gmail.com <colinfthrnghm@gmail.com>
2021-12-14 14:10:43 -0500

    updated nav

--- src/Components/MainNavbar.js
+++ src/Components/MainNavbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {auth} from './db/DBConfig'
-import {useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { Container } from 'react-bootstrap'
@@ -24,10 +24,10 @@ export const MainNavbar = ({user,totalProducts}) => {
                 {!user&&<>
                     <Navbar.Collapse id="basic-navbar-nav">
 	            	<Nav className="me-auto">
-                    <Nav.Link  href="About"> About</Nav.Link>
-                    <Nav.Link  href="Menu"> Menu</Nav.Link>
-                    <Nav.Link  href="signup">Sign Up</Nav.Link>
-                    <Nav.Link  href="login">Login</Nav.Link>
+                    <Link to="About">About</Link>
+                    <Link to="Menu"> Menu</Link>
+                    <Link to="signup">Sign Up</Link>
+                    <Link to="login">Login</Link>
                     </Nav>
                     </Navbar.Collapse>
                 </>} 
@@ -35,10 +35,10 @@ export const MainNavbar = ({user,totalProducts}) => {
                 {user&&<>
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                    <Nav.Link  href="About"> About</Nav.Link>
-                    <Nav.Link  href="Menu"> Menu</Nav.Link>
-                    <Nav.Link  href="/">Logged in: {user}</Nav.Link>
-                    <Nav.Link  href="cart"><img src="Menu-Pictures/cart.png"/> <span className='cart-indicator'>{totalProducts}</span></Nav.Link>   
+                    <Link to="About"> About</Link>
+                    <Link to="Menu"> Menu</Link>
+                    <Link to="/">Logged in: {user}</Link>
+                    <Link to="cart"><img src="Menu-Pictures/cart.png"/> <span className='cart-indicator'>{totalProducts}</span></Link>   
                     <div className='btn btn-danger btn-md'
                     onClick={handleLogout}>Logout</div>
                     </Nav>
