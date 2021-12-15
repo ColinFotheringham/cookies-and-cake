86e06618286624f824982e54da7370c34ee14546
colinfthrnghm@gmail.com <colinfthrnghm@gmail.com>
2021-12-14 19:52:48 -0500

    updating custom page

--- src/Components/MainCustom.js
+++ src/Components/MainCustom.js
@@ -1,8 +1,15 @@
 import React,{useState} from 'react'
+import emailjs from 'emailjs-com'
 
 export const MainCustom = () => {
   const [imageError, setImageError]=useState('');
   const [image, setImage]=useState(null);
+  const [Name, setName]=useState('');
+  const [Email, setEmail]=useState('');
+  const [Message, setMessage]=useState('');
+
+  const [errorMsg, setErrorMsg]=useState('');
+  const [successMsg, setSuccessMsg]=useState('');
 
   const types =['image/jpg','image/jpeg','image/png','image/PNG'];
   const handleProductImg=(e)=>{
@@ -22,20 +29,45 @@ export const MainCustom = () => {
       }
   }
 
+function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ommmyym', 'template_e9j6sf9', e.target, 'user_DG4a9l5ene8hI8UHwKvaU').then(res=>{
+        setSuccessMsg('Message Sent');
+        setName('');
+        setEmail('');
+        setMessage('');
+        setTimeout(()=>{
+            setSuccessMsg('');
+        },3000)
+    }).catch(err=> setErrorMsg(err)
+        
+);
+}
+
+
+
   return (
       <div className='container border' style={{marginTop:"50px",
       width: "55%"}}>
         <img src="Menu-Pictures/cookiesandc.jpg" className="logo mx-auto d-block banner" alt="The logo of cookies and cake" fluid />
          <h1>Custom Order Form</h1>
-      <form className='row' style={{margin:"30px 30px 30px 30px"}}>
+         {successMsg&&<>
+                <div className='success-msg'>{successMsg}</div>
+                <br></br>
+            </>}
+      <form className='row' style={{margin:"30px 30px 30px 30px"}} onSubmit={sendEmail}>
         <label>Name</label>
-        <input type="text" name="name" placeholder="John Doe" className='form-control'/>
+        <input type="text" name="name" placeholder="John Doe" className='form-control'
+         onChange={(e)=>setName(e.target.value)} value={Name}/>
 
         <label>Email</label>
-        <input type="email" name="user_email" placeholder="name@example.com" className='form-control'/>
+        <input type="email" name="user_email" placeholder="name@example.com" className='form-control'
+        onChange={(e)=>setEmail(e.target.value)} value={Email}/>
 
         <label>Custom Cake Description</label>
-        <textarea name="message" rows="4" className='form-control'/>
+        <textarea name="message" rows="4" className='form-control'
+        onChange={(e)=>setMessage(e.target.value)} value={Message}/>
         
         <label>Upload Image</label>
         <input type="file" id="file" 
@@ -45,6 +77,10 @@ export const MainCustom = () => {
         <input type = "submit" value="Send" className='form-control btn btn-success'
         style={{marginTop:'30px'}}/>
       </form>
+      {errorMsg&&<>
+                <br></br>
+                <div className='error-msg'>{errorMsg}</div>                
+            </>}
       </div> 
   )
 }
